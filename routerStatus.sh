0f3e274fc5a9d25a039c1bac60344c7821a2aed9
Marcus Hughes <hello@msh100.uk>
2020-11-29 14:56:43 +0000

    Add a read_mib function

--- routerStatus.sh
+++ routerStatus.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Take the router status from VM and produce a influx friendly version
 
-data=$(curl --silent "http://${ROUTER_IP:-192.168.100.1}/getRouterStatus")
+DATA=$(curl --silent "http://${ROUTER_IP:-192.168.100.1}/getRouterStatus")
 
 function contains() {
     local n=$#
@@ -14,10 +14,21 @@ function contains() {
     return 1
 }
 
+function read_mib() {
+    [[ "${DATA}" =~ \"${1}\":\"([^\"]+)\",? ]] && echo "${BASH_REMATCH[1]}"
+}
+
+function echoc() {
+    local input output
+    for input in "${@}"; do
+        output+="${input},"
+    done
+    echo "${output::-1}"
+}
+
 # Active bonded channels
 channelmib="1.3.6.1.4.1.4115.1.3.4.1.1.12.0"
-[[ $data  =~ \"$channelmib\":\"([0-9,]+)\" ]]
-IFS=', ' read -r -a activechannels <<< "${BASH_REMATCH[1]}"
+IFS=', ' read -r -a activechannels <<< "$(read_mib "${channelmib}")"
 
 downmibbase="1.3.6.1.2.1.10.127.1.1.1.1"
 upmibbase="1.3.6.1.2.1.10.127.1.1.2.1"
@@ -28,51 +39,26 @@ downchannels=()
 
 for channelid in "${activechannels[@]}"; do
     # Downstream channels
-    if [[ $data  =~ \"$downmibbase.1.([0-9]+)\":\"$channelid\" ]] && \
+    if [[ "${DATA}" =~ \"$downmibbase.1.([0-9]+)\":\"$channelid\" ]] && \
        ! contains "${downchannels[@]}" "${BASH_REMATCH[1]}"; then
         channel=${BASH_REMATCH[1]}
-        frequencymib="${downmibbase}.2.${channel}"
-        snrmib="${snrbase}.${channel}"
-        powermib="${downmibbase}.6.${channel}"
-        prerserrmib="${bondbase}.3.${channel}"
-        postrserrmib="${bondbase}.4.${channel}"
         downchannels+=("${channel}")
 
-        [[ $data  =~ \"$frequencymib\":\"(-?[0-9]+)\" ]]
-        frequency=${BASH_REMATCH[1]}
-        [[ $data  =~ \"$snrmib\":\"(-?[0-9]+)\" ]]
-        snr=${BASH_REMATCH[1]}
-        [[ $data  =~ \"$powermib\":\"(-?[0-9]+)\" ]]
-        power=${BASH_REMATCH[1]}
-        [[ $data  =~ \"$prerserrmib\":\"(-?[0-9]+)\" ]]
-        prerserr=${BASH_REMATCH[1]}
-        [[ $data  =~ \"$postrserrmib\":\"(-?[0-9]+)\" ]]
-        postrserr=${BASH_REMATCH[1]}
-
         echo -n "downstream,channel=${channel},id=${channelid} "
-        echo \
-            "frequency=${frequency}" \
-            "snr=${snr}" \
-            "power=${power}" \
-            "prerserr=${prerserr}" \
-            "postrserr=${postrserr}" \
-            | tr ' ' ','
+        echoc \
+            "frequency=$(read_mib "${downmibbase}.2.${channel}")" \
+            "snr=$(read_mib "${snrbase}.${channel}")" \
+            "power=$(read_mib "${downmibbase}.6.${channel}")" \
+            "prerserr=$(read_mib "${bondbase}.3.${channel}")" \
+            "postrserr=$(read_mib "${bondbase}.4.${channel}")"
     # Upstream channels
-    elif [[ $data  =~ \"$upmibbase.1.([0-9]+)\":\"$channelid\" ]]; then
+    elif [[ "${DATA}" =~ \"$upmibbase.1.([0-9]+)\":\"$channelid\" ]]; then
         channel=${BASH_REMATCH[1]}
-        frequencymib="${upmibbase}.2.${channel}"
-        powermib="${powerbase}.${channel}"
-
-        [[ $data  =~ \"$frequencymib\":\"(-?[0-9]+)\" ]]
-        frequency=${BASH_REMATCH[1]}
-        [[ $data  =~ \"$powermib\":\"(-?[0-9]+)\" ]]
-        power=${BASH_REMATCH[1]}
 
         echo -n "upstream,channel=${channel},id=${channelid} "
-        echo \
-            "frequency=${frequency}" \
-            "power=${power}" \
-            | tr ' ' ','
+        echoc \
+            "frequency=$(read_mib "${upmibbase}.2.${channel}")" \
+            "power=$(read_mib "${powerbase}.${channel}")"
     fi
 done
 
@@ -82,24 +68,20 @@ downstreamid="1.3.6.1.4.1.4491.2.1.21.1.3.1.8.2.2."
 ratemib="1.3.6.1.4.1.4491.2.1.21.1.2.1.6.2.1"
 burstmib="1.3.6.1.4.1.4491.2.1.21.1.2.1.7.2.1"
 
-if [[ $data  =~ \"$upstreamid([0-9]+)\":\"1\" ]]; then
+if [[ "${DATA}" =~ \"$upstreamid([0-9]+)\":\"1\" ]]; then
     upstreamid=${BASH_REMATCH[1]}
 
-    [[ $data  =~ \"$ratemib.$upstreamid\":\"([0-9]+)\" ]]
-    maxrate=${BASH_REMATCH[1]}
-    [[ $data  =~ \"$burstmib.$upstreamid\":\"([0-9]+)\" ]]
-    maxburst=${BASH_REMATCH[1]}
-
-    echo "config,config=upstream maxrate=${maxrate},maxburst=${maxburst}"
+    echo -n "config,config=upstream "
+    echoc \
+        "maxrate=$(read_mib "${ratemib}.${upstreamid}")" \
+        "maxburst=$(read_mib "${burstmib}.${upstreamid}")"
 fi
 
-if [[ $data  =~ \"$downstreamid([0-9]+)\":\"1\" ]]; then
+if [[ "${DATA}" =~ \"$downstreamid([0-9]+)\":\"1\" ]]; then
     downstreamid=${BASH_REMATCH[1]}
 
-    [[ $data  =~ \"$ratemib.$downstreamid\":\"([0-9]+)\" ]]
-    maxrate=${BASH_REMATCH[1]}
-    [[ $data  =~ \"$burstmib.$downstreamid\":\"([0-9]+)\" ]]
-    maxburst=${BASH_REMATCH[1]}
-
-    echo "config,config=downstream maxrate=${maxrate},maxburst=${maxburst}"
+    echo -n "config,config=downstream "
+    echoc \
+        "maxrate=$(read_mib "${ratemib}.${downstreamid}")" \
+        "maxburst=$(read_mib "${burstmib}.${downstreamid}")"
 fi
